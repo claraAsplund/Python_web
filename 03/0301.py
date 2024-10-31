8e37ca7910d40dcd9d95b512248aa78f3c244464
Clara Asplund <clarasplund@gmail.com>
2024-10-31 13:33:52 +0100

    Python websrc

---
+++ 03/0301.py
@@ -0,0 +1,26 @@
+winner = 50
+
+if winner  != 10:
+    print("if")
+elif winner <=25:
+    print("elif")
+else:
+    print("else")
+
+
+"""
+Conditional : If you need money, so go to ATM
+there is a condition 1:if the 4 password is correct give to customer the money,
+2: otherwise showing "ERROR"
+
+Conditional allows us to handle different scenarios
+
+
+if condition: if the condition is true
+    "write the code to run"
+
+
+a = 10 
+we r gonna put 10 inside of "a"variable
+10 == 10 is checking left 10 and right 10 is the same  
+"""
---
+++ 03/0302.py
@@ -0,0 +1,21 @@
+winner = 10
+
+if winner > 10:
+    print("Winner is greater than 10")
+
+elif winner < 10:
+    print("winner is less than 10")
+
+else:
+    print("Winner is 10")
+
+
+"""
+elif allows you provide another alternative for your code
+plus a condition
+
+else just allows you alternative when "if" is not TRUE
+
+
+if, elss, elif allow you excecute conditionally
+"""
